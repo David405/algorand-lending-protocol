6f180c147382b9c82b2aa9f720cf1732a44f514a
ifebrand6 <ifebrand6@gmail.com>
2022-02-10 00:33:34 +0100

    add tranactions

--- src/pages/transactions/transactions.jsx
+++ src/pages/transactions/transactions.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Card } from "../../components/elements/card";
 export function Transactions(params) {
+  const transactions = 0
+  const transactions_result = (transactions == 0) ? <><p className="mx-auto pt-5 text-white-50">No transactions done yet.</p></> : <> <div className="col-xl-7 col-lg-7">
+  <Card
+  // components={<Button  onClick={getTransactionHistory} label={"get transactions"} className="generate-btn"/>} 
+  />
+</div>
+</>
   return (
     <div className="row">
-      <div className="col-xl-7 col-lg-7">
-        <Card
-        // components={<Button  onClick={getTransactionHistory} label={"get transactions"} className="generate-btn"/>} 
-        />
-      </div>
-      <div className="col-xl-5 col-lg-5">
-        <Card/>
-      </div>
+      {transactions_result}
     </div>
   );
 }
